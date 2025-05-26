3f6b8dd0e1fca8defed4d490b3946d641a64a893
kevo-ship-it <familygrouploanandsavings@gmail.com>
2025-05-26 21:34:25 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,1 +1,16 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/lib/theme-provider";
 
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider defaultTheme="dark">
+      <App />
+      <Toaster />
+    </ThemeProvider>
+  </QueryClientProvider>
+);
