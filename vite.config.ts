f2ca4fa714ff69fc8af29fdf83fa9cca57e45557
kevo-ship-it <familygrouploanandsavings@gmail.com>
2025-05-26 23:59:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,20 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  root: "./client",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client/src"),
       "@shared": path.resolve(__dirname, "./shared"),
-      "@assets": path.resolve(__dirname, "./client/assets"),
     },
+  },
+  build: {
+    outDir: "../dist/client",
+    emptyOutDir: true,
   },
   server: {
     host: "0.0.0.0",
     port: 3000,
-  },
-  build: {
-    outDir: "dist/client",
   },
 });
