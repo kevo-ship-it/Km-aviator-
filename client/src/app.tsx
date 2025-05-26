add7ea16452b48a4af51e9ed6769e4830d5a432b
kevo-ship-it <familygrouploanandsavings@gmail.com>
2025-05-26 21:35:18 +0300

    Update app.tsx

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,1 +1,29 @@
+import { Switch, Route } from "wouter";
+import Home from "@/pages/home";
+import NotFound from "@/pages/not-found";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AuthProvider } from "@/hooks/use-auth";
+import { GameProvider } from "@/hooks/use-game";
 
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <GameProvider>
+        <TooltipProvider>
+          <Router />
+        </TooltipProvider>
+      </GameProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
