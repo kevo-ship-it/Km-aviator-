03de977c957f9bae0235439d8fe1ed40cfa99625
kevo-ship-it <familygrouploanandsavings@gmail.com>
2025-05-29 21:24:38 +0300

    Create mobile-navigation.tsx

---
+++ client/src/components/mobile-navigation.tsx
@@ -0,0 +1,9 @@
+export default function MobileNavigation() {
+  return (
+    <div className="md:hidden bg-neutral-darker border-t border-neutral-dark p-4">
+      <div className="text-center text-gray-400">
+        Mobile navigation placeholder
+      </div>
+    </div>
+  );
+}
