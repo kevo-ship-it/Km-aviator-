0f310d0e49715834448d224a6257ccd6bea4ce7f
kevo-ship-it <familygrouploanandsavings@gmail.com>
2025-06-08 14:06:22 +0300

    Update supabase.ts

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -3,11 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-    storage: localStorage,
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
